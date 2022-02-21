c63102132e21c0667ebc8a94fe76909706081ffe
Liam Desmond <liamjdesmond84@gmail.com>
2022-02-21 11:49:07 -0600

    Updating Appt

---
+++ src/main/java/com/liam/kafkaintro/KafkaListeners.java
@@ -0,0 +1,14 @@
+package com.liam.kafkaintro;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KafkaListeners {
+	
+	@KafkaListener(topics = "kafkaintro", groupId = "groupId") // for more listeners you want want to change the groupId to be unique
+	void listener(String data) {
+		System.out.println("Listener recieved " + data);
+	}
+
+}
